d6de6b14fb13b4edf7b789445b3c9f663ae93ff4
Khayal-Dev <khayaldev1@gmail.com>
2024-06-21 14:21:07 +0300

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import "@/styles/globals.css";
+import "@mantine/core/styles.css";
+
 import type { AppProps } from "next/app";
+import { createTheme, MantineProvider } from "@mantine/core";
+
+const theme = createTheme({
+  /** Mantine theme override here */
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider theme={theme}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
